7f7126cdf3ec5399642cac6053b603562b842443
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-03-13 18:40:51 +0530

    [cc] Add custom-sign in flow

--- apps/collector-console/app/(dashboard)/page.tsx
+++ apps/collector-console/app/(dashboard)/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <div>Welcome to Dashobard Anonymous User</div>;
+import { SignOutButton, UserButton } from "@clerk/nextjs";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+export default async function Page() {
+  const { userId } = await auth();
+
+  if (!userId) throw Error("No user");
+  const { users } = await clerkClient();
+  const { fullName, emailAddresses } = await users.getUser(userId);
+  return (
+    <div className="flex flex-col gap-8 items-center justify-center text-2xl h-svh">
+      <UserButton />
+      Welcome to Dashobard {fullName ?? emailAddresses.at(0)?.emailAddress}
+    </div>
+  );
 }
--- apps/collector-console/components/signin-form.tsx
+++ apps/collector-console/components/signin-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { cn } from "@cmt/ui/lib/utils";
 import { Button } from "@cmt/ui/components/button";
 import {
@@ -8,12 +9,64 @@ import {
   CardTitle,
 } from "@cmt/ui/components/card";
 import { Input } from "@cmt/ui/components/input";
-import { Label } from "@cmt/ui/components/label";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@cmt/ui/components/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSignIn } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
+import { Loader2Icon } from "lucide-react";
+
+const signInSchema = z.object({
+  email: z.string().trim().min(1, {
+    message: "Email address must be fill to continue.",
+  }),
+  password: z.string().trim().min(1, {
+    message: "Password must be fill to continue.",
+  }),
+});
 
 export function SignInForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get("redirect_url");
+
+  async function onSubmit(values: z.infer<typeof signInSchema>) {
+    try {
+      if (isLoaded) {
+        const { createdSessionId, status } = await signIn.create({
+          password: values.password,
+          identifier: values.email,
+          redirectUrl: redirectUrl ?? undefined,
+        });
+
+        if (status === "complete") setActive({ session: createdSessionId });
+      }
+    } catch (e) {
+      console.log(e);
+      form.setError("root", { message: "Email or password is incorrect" });
+    }
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="bg-transparent border-none shadow-none">
@@ -22,42 +75,72 @@ export function SignInForm({
           <CardDescription>
             Enter your email below to login to your account
           </CardDescription>
+          <p className="text-sm text-destructive text-sm">
+            {form.formState.errors.root?.message}
+          </p>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email address</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="flex flex-col gap-6">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  disabled={form.formState.isSubmitting || !isLoaded}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="email">Email address</FormLabel>
+                      <FormControl>
+                        <Input placeholder="m@example.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
                 />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    disabled={form.formState.isSubmitting || !isLoaded}
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className="flex items-center">
+                          <FormLabel htmlFor="password">Password</FormLabel>
+                          <a
+                            href="#"
+                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                          >
+                            Forgot your password?
+                          </a>
+                        </div>
+                        <FormControl>
+                          <Input placeholder="•••••••••••••••••" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-                <Input id="password" type="password" required />
+                <div className="flex flex-col gap-3">
+                  <Button
+                    disabled={form.formState.isSubmitting || !isLoaded}
+                    type="submit"
+                    className="w-full"
+                  >
+                    {form.formState.isSubmitting ? (
+                      <Loader2Icon className="animate-spin" />
+                    ) : (
+                      "Continue"
+                    )}
+                  </Button>
+                </div>
               </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
-                  Continue
-                </Button>
+              <div className="mt-4 text-center text-sm">
+                By clicking continue, you agree to our Terms of Service and
+                Privacy Policy.
               </div>
-            </div>
-            <div className="mt-4 text-muted-foreground text-center text-sm">
-              By clicking continue, you agree to our Terms of Service and
-              Privacy Policy.
-            </div>
-          </form>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
