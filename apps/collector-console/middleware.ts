a44faf000348d829a8d5c3e1966c3d2243a4fe7f
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-03-17 23:59:38 +0530

    chore (env) Add new clerk env variables

--- apps/collector-console/middleware.ts
+++ apps/collector-console/middleware.ts
@@ -5,37 +5,32 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 const isUploadthingRoute = createRouteMatcher(["/api/uploadthing(.*)"]);
 const isOnboardingRoute = createRouteMatcher(["/onboarding"]);
 
-export default clerkMiddleware(
-  async (auth, req: NextRequest) => {
-    const { userId, sessionClaims, redirectToSignIn } = await auth();
-    //For uploading files, don't try to redirect
-    if (isUploadthingRoute(req)) {
-      return NextResponse.next();
-    }
-
-    // For users visiting /onboarding, don't try to redirect
-    if (userId && isOnboardingRoute(req)) {
-      return NextResponse.next();
-    }
-
-    // If the user isn't signed in and the route is private, redirect to sign-in
-    if (!userId && !isPublicRoute(req))
-      return redirectToSignIn({ returnBackUrl: req.url });
-
-    // Catch users who do not have `onboardingComplete: true` in their publicMetadata
-    // Redirect them to the /onboading route to complete onboarding
-    if (userId && !sessionClaims?.metadata?.onboardingComplete) {
-      const onboardingUrl = new URL("/onboarding", req.url);
-      return NextResponse.redirect(onboardingUrl);
-    }
-
-    // If the user is logged in and the route is protected, let them view.
-    if (userId && !isPublicRoute(req)) return NextResponse.next();
-  },
-  {
-    signInUrl: "/sign-in",
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { userId, sessionClaims, redirectToSignIn } = await auth();
+  //For uploading files, don't try to redirect
+  if (isUploadthingRoute(req)) {
+    return NextResponse.next();
   }
-);
+
+  // For users visiting /onboarding, don't try to redirect
+  if (userId && isOnboardingRoute(req)) {
+    return NextResponse.next();
+  }
+
+  // If the user isn't signed in and the route is private, redirect to sign-in
+  if (!userId && !isPublicRoute(req))
+    return redirectToSignIn({ returnBackUrl: req.url });
+
+  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
+  // Redirect them to the /onboading route to complete onboarding
+  if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+    const onboardingUrl = new URL("/onboarding", req.url);
+    return NextResponse.redirect(onboardingUrl);
+  }
+
+  // If the user is logged in and the route is protected, let them view.
+  if (userId && !isPublicRoute(req)) return NextResponse.next();
+});
 
 export const config = {
   matcher: [
